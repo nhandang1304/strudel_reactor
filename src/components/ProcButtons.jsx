14e756b5af9d607d0e015d842eac1b4719f1957c
nhandang1304 <danty035@mymail.unisa.edu.au>
2025-10-23 11:06:15 +1030

    Seperate some functions into different components (DJ Control, Play and Stop Buttons, Preprocess Button, Proc Button)

---
+++ src/components/ProcButtons.jsx
@@ -0,0 +1,13 @@
+function ProcButtons() {
+    return (
+        <>
+            <div className="btn-group" role="group" ara-label="Basic mixed styles example!">
+                <button id="process" className="btn btn-outline-primary">Preprocess</button>
+                <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+            </div>
+
+        </>
+       
+    )
+}
+export default ProcButtons
