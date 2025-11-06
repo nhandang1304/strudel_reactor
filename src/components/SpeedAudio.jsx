93e0e5bd186d5fe6f5477e7047782beadb4cb097
nhandang1304 <danty035@mymail.unisa.edu.au>
2025-11-06 15:05:42 +1030

    Add onClick for each Speed button

--- src/components/SpeedAudio.jsx
+++ src/components/SpeedAudio.jsx
@@ -1,24 +1,25 @@
 import { RiSpeedFill } from "react-icons/ri";
 import { MdSlowMotionVideo } from "react-icons/md";
 import Tooltips from "./TooltipDes"
-function SpeedAudio() {
+import SpeedLogic from "../utils/SpeedLogic"
+function SpeedAudio({globalEditor }) {
     return (
         <>
             <div className="d-flex gap-2">
                 <Tooltips title="x2.0 Speed Audio">
-                    <button className="btn btn-light fw-bold">X2 <RiSpeedFill size="20"/> </button>
+                    <button onClick={()=> SpeedLogic(2, globalEditor.current) } className="btn btn-light fw-bold">X2 <RiSpeedFill size="20"/> </button>
                 </Tooltips>
                 <Tooltips title="x4.0 Speed Audio">
-                    <button className="btn btn-light fw-bold">X4 <RiSpeedFill size="20" /></button>
+                    <button onClick={() => SpeedLogic(4, globalEditor.current)} className="btn btn-light fw-bold">X4 <RiSpeedFill size="20" /></button>
                 </Tooltips>
                 <Tooltips title="Normal Speed Audio">
-                    <button className="btn btn-light fw-bold">Normal (X1) </button>
+                    <button onClick={() => SpeedLogic(2, globalEditor.current)} className="btn btn-light fw-bold">Normal (X1) </button>
                 </Tooltips>
                 <Tooltips title="x0.5 Speed Audio">
-                    <button className="btn btn-light fw-bold">X0.5 <MdSlowMotionVideo size="20" /></button>
+                    <button onClick={() => SpeedLogic(0.5, globalEditor.current)} className="btn btn-light fw-bold">X0.5 <MdSlowMotionVideo size="20" /></button>
                 </Tooltips>
                 <Tooltips title="x0.25 Speed Audio">
-                    <button className="btn btn-light fw-bold">X0.25 <MdSlowMotionVideo size="20" /></button>
+                    <button onClick={() => SpeedLogic(0.5, globalEditor.current)} className="btn btn-light fw-bold">X0.25 <MdSlowMotionVideo size="20" /></button>
                 </Tooltips>
             </div>
           
